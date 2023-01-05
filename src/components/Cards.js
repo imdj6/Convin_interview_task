3faec26cd180370c04cec42b5b52233036503e9a
MD DANISH JAMAL <mddanishjamal04@yahoo.com>
2023-01-05 15:57:33 +0530

    button fix

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,7 @@ function Cards(props) {
     const [modal,setModal]=useState(false);
     const [id,setId]=useState(props.id);
     return (
-        <div className='bg-gray-500 text-white w-40 flex flex-col border border-black rounded-2xl'>
+        <div className='bg-gray-500 text-white w-52 flex flex-col border border-black rounded-2xl'>
             {
                 modal && (
                     <Modal>
@@ -15,7 +15,7 @@ function Cards(props) {
             }
             <div><img src={props.avatar} className='rounded-full mx-auto mt-2' width={60} /></div>
             <div className='mx-auto mt-3 text-lg font-bold'>{props.first_name}</div>
-            <button className='bg-[#6A5FEB] p-1.5 mt-4 mb-3 mx-auto hover:scale-95' onClick={()=>{
+            <button className='bg-[#6A5FEB] p-1 mt-4 mb-3 mx-auto hover:scale-95 rounded-md' onClick={()=>{
                 setModal(true);
             }}>
                 Know More
